a9ec964be35907ca0a987e642bbd7646fe8c5cd5
DeanNguyen98 <nctuan.hn@gmail.com>
2024-04-03 21:36:01 +1100

    Test for calculator after adding if statement

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,6 @@
 const calc = require("../code/calculator");
 const calculator = new calc(5,6);
+const specialcalc = new calc(6, undefined);
 
 test("Add", () => {
     expect(calculator.add()).toBe(11)
@@ -10,4 +11,12 @@ test("Minus" ,() => {
 
 test("Divide", () => {
     expect(calculator.divide()).toBe(0.83);
+})
+
+test("no first operand add ", () => {
+    expect(specialcalc.add()).toBe(6)
+})
+
+test("no first operand divide", () => {
+    expect(specialcalc.divide()).toBe(Infinity)
 })
